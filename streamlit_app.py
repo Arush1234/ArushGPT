a603deed89da8117705e3128fb883f5e238982c0
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2023-06-24 16:53:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from langchain.llms import OpenAI
 
-st.title('🎈 App Name')
+st.title(ArushGPT ⚡)
+openai_api_key = st.sidebar.text_input('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')
 
-st.write('Hello world!')
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter OpenAI API key!', icon='🚨')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
